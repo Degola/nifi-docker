2b3a9679be74edf4c756c822c93600bb1f64705b
Sebastian Lagemann <sebastian.lagemann@iqu.com>
2016-11-10 20:39:49 +0100

    added nifi standard nar bundle updated to jolt 0.0.24

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker build -t degola/nifi:1.0.0-beta -t degola/nifi:latest .
+docker build -t degola/nifi:1.0.0 -t degola/nifi:latest .
 
+docker push degola/nifi:1.0.0
+docker push degola/nifi:latest
